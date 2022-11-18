8ddda1dc2bcc5f91e939e0a8a11367b35b1e292e
Mazettt <martin.d-herouville@epitech.eu>
2022-11-18 10:43:21 +0100

    (|) update verif auth

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -4,13 +4,13 @@ const SECRET = glob.myenv.SECRET;
 
 module.exports = async function(app, con) {
     app.get("/user", glob.verifyToken, async (req, res) => {
-        if (glob.verifyAuth(req, res, false)) {
-            con.query(`SELECT * FROM user;`, function (err2, rows) {
-                if (err2) res.status(500).json({ msg: "Internal server error" });
-                res.send(rows);
-            });
-        } else if (!res.headersSent) {
-            res.status(403).json({ msg: "Authorization denied" });
+        if (!glob.verifyAuth(req, res, false)) {
+            !res.headersSent ? res.status(403).json({ msg: "Authorization denied" }) : 0;
+            return;
         }
+        con.query(`SELECT * FROM user;`, function (err2, rows) {
+            if (err2) res.status(500).json({ msg: "Internal server error" });
+            res.send(rows);
+        });
     });
 }
--- src/routes/user/user_id.js
+++ src/routes/user/user_id.js
@@ -2,17 +2,17 @@ var glob = require('../../global');
 
 module.exports = async function(app, con) {
     app.get("/user/:id", glob.verifyToken, async (req, res) => {
-        if (glob.verifyAuth(req, res, true)) {
-            con.query(`SELECT * FROM user WHERE id = "${req.params.id}" OR email = "${req.params.id}";`, function (err, rows) {
-                if (err) res.status(500).json({ msg: "Internal server error" });
-                if (rows[0])
-                    res.send(rows[0]);
-                else
-                    res.status(200).json({ msg: "Not found"});
-            });
-        } else if (!res.headersSent) {
-            res.status(403).json({ msg: "Authorization denied" });
+        if (!glob.verifyAuth(req, res, true)) {
+            !res.headersSent ? res.status(403).json({ msg: "Authorization denied" }) : 0;
+            return;
         }
+        con.query(`SELECT * FROM user WHERE id = "${req.params.id}" OR email = "${req.params.id}";`, function (err, rows) {
+            if (err) res.status(500).json({ msg: "Internal server error" });
+            if (rows[0])
+                res.send(rows[0]);
+            else
+                res.status(200).json({ msg: "Not found"});
+        });
     });
 
     app.put("/user/:id", glob.verifyToken, async (req, res) => {
@@ -20,30 +20,30 @@ module.exports = async function(app, con) {
             res.status(400).json({ msg: "Bad parameter" });
             return;
         }
-        if (glob.verifyAuth(req, res, true)) {
-            var ret = false;
-            if (req.body.hasOwnProperty('email')) {
-                con.query(`UPDATE user SET email = "${req.body.email}" WHERE id = "${req.params.id}";`, function (err, result) {
-                    if (err) res.status(500).json({ msg: "Internal server error" });
-                });
-                ret = true;
-            }
-            if (req.body.hasOwnProperty('password')) {
-                con.query(`UPDATE user SET password = "${req.body.password}" WHERE id = "${req.params.id}";`, function (err, result) {
-                    if (err) res.status(500).json({ msg: "Internal server error" });
-                });
-                ret = true;
-            }
-            if (ret == true) {
-                con.query(`SELECT * FROM user WHERE id = "${req.params.id}";`, function (err, rows) {
-                    if (err) res.status(500).json({ msg: "Internal server error" });
-                    res.status(200).send(rows);
-                });
-            } else
-                res.status(400).json({ msg: "Bad parameter" });
-        } else if (!res.headersSent) {
-            res.status(403).json({ msg: "Authorization denied" });
+        if (!glob.verifyAuth(req, res, true)) {
+            !res.headersSent ? res.status(403).json({ msg: "Authorization denied" }) : 0;
+            return;
         }
+        var ret = false;
+        if (req.body.hasOwnProperty('email')) {
+            con.query(`UPDATE user SET email = "${req.body.email}" WHERE id = "${req.params.id}";`, function (err, result) {
+                if (err) res.status(500).json({ msg: "Internal server error" });
+            });
+            ret = true;
+        }
+        if (req.body.hasOwnProperty('password')) {
+            con.query(`UPDATE user SET password = "${req.body.password}" WHERE id = "${req.params.id}";`, function (err, result) {
+                if (err) res.status(500).json({ msg: "Internal server error" });
+            });
+            ret = true;
+        }
+        if (ret == true) {
+            con.query(`SELECT * FROM user WHERE id = "${req.params.id}";`, function (err, rows) {
+                if (err) res.status(500).json({ msg: "Internal server error" });
+                res.status(200).send(rows);
+            });
+        } else
+            res.status(400).json({ msg: "Bad parameter" });
     });
 
     app.delete("/user/:id", glob.verifyToken, async (req, res) => {
@@ -51,16 +51,16 @@ module.exports = async function(app, con) {
             res.status(400).json({ msg: "Bad parameter" });
             return;
         }
-        if (glob.verifyAuth(req, res, true)) {
-            con.query(`DELETE FROM user WHERE id = "${req.params.id}";`, function (err, result) {
-                if (err) res.status(500).json({ msg: "Internal server error" });
-                if (result.affectedRows != 0)
-                    res.status(200).json({ msg: `Successfully deleted record number: ${req.params.id}` });
-                else
-                    res.status(200).json({ msg: "Not found"});
-            });
-        } else if (!res.headersSent) {
-            res.status(403).json({ msg: "Authorization denied" });
+        if (!glob.verifyAuth(req, res, true)) {
+            !res.headersSent ? res.status(403).json({ msg: "Authorization denied" }) : 0;
+            return;
         }
+        con.query(`DELETE FROM user WHERE id = "${req.params.id}";`, function (err, result) {
+            if (err) res.status(500).json({ msg: "Internal server error" });
+            if (result.affectedRows != 0)
+                res.status(200).json({ msg: `Successfully deleted record number: ${req.params.id}` });
+            else
+                res.status(200).json({ msg: "Not found"});
+        });
     });
 }
