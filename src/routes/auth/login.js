c3ffeb45e195b9abd35756c182657780890571f0
redboard <ott.thomas68@gmail.com>
2022-12-15 20:19:48 +0100

    Revert "(+) /login return email"
    This reverts commit 9714223e37b5220a60b7efe3e8be5033eae28fbb.

--- src/routes/auth/login.js
+++ src/routes/auth/login.js
@@ -26,7 +26,7 @@ module.exports = async function(app, con) {
             } else {
                 if (bcrypt.compareSync(req.body.password, rows[0].password)) {
                     var token = jwt.sign({ id: `${rows[0].id}` }, process.env.SECRET, { expiresIn: '24h' });
-                    res.status(201).json({token: token, id: rows[0].id, email: rows[0].email});
+                    res.status(201).json({token: token, id: rows[0].id});
                 } else
                     res.status(400).json({ msg: "Invalid Credentials" });
             }
